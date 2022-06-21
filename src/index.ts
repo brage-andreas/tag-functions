d842d2cbfdc745d3859105ae6a14235cb21050dd
Drango <draaangooo@gmail.com>
2022-06-21 12:38:22 +0200

    refactor: format

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,18 @@ export function createTag<T>(fn: (string: string) => T): {
 	(string: ReadonlyArray<string>, ...keys: ReadonlyArray<unknown>): T;
 } {
 	function tagFn(string: string): T;
-	function tagFn(string: ReadonlyArray<string>, ...keys: ReadonlyArray<unknown>): T;
-	function tagFn(string: ReadonlyArray<string> | string, ...keys: ReadonlyArray<unknown>): T {
+	function tagFn(
+		string: ReadonlyArray<string>,
+		...keys: ReadonlyArray<unknown>
+	): T;
+	function tagFn(
+		string: ReadonlyArray<string> | string,
+		...keys: ReadonlyArray<unknown>
+	): T {
 		const stringArray = typeof string === "string" ? [string] : string;
-		const joinedStringArray = stringArray.map((str, i) => `${str}${keys[i] ?? ""}`);
+		const joinedStringArray = stringArray.map(
+			(str, i) => `${str}${keys[i] ?? ""}`
+		);
 
 		return fn(joinedStringArray.join(""));
 	}
