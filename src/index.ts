1b32d2ef7e925eed16f2f7c1eb13cd87733e8bc4
Drango <draaangooo@gmail.com>
2022-06-21 13:08:48 +0200

    fix: export default

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-export function createTag<T>(fn: (string: string) => T): {
+export default function createTag<T>(fn: (string: string) => T): {
 	(string: string): T;
 	(string: ReadonlyArray<string>, ...keys: ReadonlyArray<unknown>): T;
 } {
