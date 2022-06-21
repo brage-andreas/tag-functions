b64f4e6f5eaf345ed769c9a1cd3ecf2d48414acd
Drango <draaangooo@gmail.com>
2022-06-21 13:51:38 +0200

    fix: make export default

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,5 +1,5 @@
 declare module "@drango/tag-functions" {
-	export function createTag<T>(fn: (string: string) => T): {
+	export default function createTag<T>(fn: (string: string) => T): {
 		(string: string): T;
 		(string: ReadonlyArray<string>, ...keys: ReadonlyArray<unknown>): T;
 	};
