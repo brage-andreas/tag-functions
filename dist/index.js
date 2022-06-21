7a2a106c17610def976fe5ca0be756e1dd91a820
Drango <draaangooo@gmail.com>
2022-06-21 13:08:38 +0200

    build

--- dist/index.js
+++ dist/index.js
@@ -1,8 +1,8 @@
 export default function createTag(fn) {
     function tagFn(string, ...keys) {
-        const stringArr = typeof string === "string" ? [string] : string;
-        const joinedArr = stringArr.map((str, i) => `${str}${keys[i] ?? ""}`);
-        return fn(joinedArr.join(""));
+        const stringArray = typeof string === "string" ? [string] : string;
+        const joinedStringArray = stringArray.map((str, i) => `${str}${keys[i] ?? ""}`);
+        return fn(joinedStringArray.join(""));
     }
     return tagFn;
 }
